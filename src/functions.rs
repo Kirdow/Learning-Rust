69db310c6d69225bb06ed24186e18c19be8dca1e
Dennis Rönn <10155603+Kirdow@users.noreply.github.com>
2022-05-19 19:35:12 +0200

    add closure demo

--- src/functions.rs
+++ src/functions.rs
@@ -6,6 +6,11 @@ pub fn run() {
     // Bind fuunction values to variables
     let get_sum = add(5, 7);
     println!("Sum: {}", get_sum);
+
+    // Closure
+    let n3: i32 = 10;
+    let add_nums = |n1: i32, n2: i32| (n1 + n2) * n3;
+    println!("C Sum: {}", add_nums(3, 5));
 }
 
 fn greeting(greet: &str, name: &str) {
