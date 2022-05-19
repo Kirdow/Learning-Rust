3aa82e42f1bcbc312aab9e26aad8fba20da62179
Dennis Rönn <10155603+Kirdow@users.noreply.github.com>
2022-05-19 19:24:50 +0200

    add infinite loop demo

--- src/loops.rs
+++ src/loops.rs
@@ -1,5 +1,15 @@
 // Loops - Used to iterate until a condition is met
 
 pub fn run() {
-    
+    let mut count = 0;
+
+    // Infinite Loop
+    loop {
+        count += 1;
+        println!("Number {}", count);
+
+        if count == 20 {
+            break;
+        }
+    }
 }
