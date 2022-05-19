3ced96ec92e845711decb9bdffb1bedde295553c
Dennis Rönn <10155603+Kirdow@users.noreply.github.com>
2022-05-19 18:31:27 +0200

    add array mutability demo

--- src/arrays.rs
+++ src/arrays.rs
@@ -1,7 +1,10 @@
 // Arrays - Fixed list where elements are the same data types
 
 pub fn run() {
-    let numbers: [i32; 5] = [1, 2, 3, 4, 5];
+    let mut numbers: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // Re-assign value
+    numbers[2] = 20;
 
     println!("{:?}", numbers);
 
