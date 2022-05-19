e780a5bbc22b91293af180f25a4a227d11bfa81b
Dennis Rönn <10155603+Kirdow@users.noreply.github.com>
2022-05-19 17:08:36 +0200

    add print formatting

--- src/print.rs
+++ src/print.rs
@@ -1,4 +1,22 @@
 pub fn run() {
     // Print to console
     println!("This is run from print.rs!");
+
+    // Basic formatting
+    println!("{}, {} is from {}", "Kirdow", 24, "Sweden");
+
+    // Positional arguments
+    println!("{0} is from {1} and {0} is {2} yo", "Kirdow", "Sweden", 24);
+
+    // Named arguments
+    println!("{name} likes to play {activity}", name = "Kirdow", activity = "Video Games");
+
+    // Placeholder traits
+    println!("Binary: {:b} Hex: {:x} Octal: {:o}", 10, 10, 10);
+
+    // Placeholder traits + Positional arguments
+    println!("Binary: {0:b} Hex: {0:x} Octal: {0:o}", 10);
+
+    // Placeholder for debug trait
+    println!("{:?}", (12, true, "hello"));
 }
