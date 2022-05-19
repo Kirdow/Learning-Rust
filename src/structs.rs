44048af01af20736059b0f6c081a6472c1f2ed22
Dennis Rönn <10155603+Kirdow@users.noreply.github.com>
2022-05-19 19:49:50 +0200

    add struct creation demo

--- src/structs.rs
+++ src/structs.rs
@@ -1,5 +1,14 @@
 // Structs - Used to create custom data types
 
-pub fn run() {
+// Traditional Struct
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8
+}
 
+pub fn run() {
+    let c = Color { red: 255, green: 0, blue: 0 };
+
+    println!("Color: {} {} {}", c.red, c.green, c.blue);
 }
