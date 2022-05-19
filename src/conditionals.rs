8ab1786f86ac164731b9bbb119694b3ff91c653a
Dennis Rönn <10155603+Kirdow@users.noreply.github.com>
2022-05-19 19:18:02 +0200

    add else if demo

--- src/conditionals.rs
+++ src/conditionals.rs
@@ -4,10 +4,14 @@ pub fn run() {
     const LEGAL_AGE: i32 = 18;
     let age = 16;
 
+    let check_id = false;
+
     // If/Else
-    if age >= LEGAL_AGE {
+    if age >= LEGAL_AGE && check_id {
         println!("Bartender: What would you like to drink?");
-    } else {
+    } else if age < LEGAL_AGE && check_id {
         println!("Bartender: Sorry bud, need to be {}. Come back in {} years!", LEGAL_AGE, LEGAL_AGE - age);
+    } else {
+        println!("Bartender: I'll need to see your ID");
     }
 }
