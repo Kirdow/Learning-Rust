899ae869854c649ffbbac2e4e7550b59ebe6ec51
Dennis Rönn <10155603+Kirdow@users.noreply.github.com>
2022-05-19 18:04:45 +0200

    add strings demo

--- src/strings.rs
+++ src/strings.rs
@@ -2,5 +2,16 @@
 // String = Growable, heap-allocated data structure - Use when you need to modify or own string data
 
 pub fn run() {
+    // Primitive str
+    let my_str = "My Str";
 
+    println!("Primitive [my_str] = {}", my_str);
+
+    // Mutable str
+    let hello = String::from("Hello");
+
+    println!("Mutable String [hello] = {}", hello);
+
+    // Get length
+    println!("Length: {}", hello.len());
 }
