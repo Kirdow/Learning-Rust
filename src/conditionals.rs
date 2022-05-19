e6d0c11c61720c403ece1b7e3d2bcd1c2770e7d5
Dennis Rönn <10155603+Kirdow@users.noreply.github.com>
2022-05-19 19:14:51 +0200

    add if else bartender demo

--- src/conditionals.rs
+++ src/conditionals.rs
@@ -1,5 +1,13 @@
 // Conditionals - Used to check the condition of something and act on the result
 
 pub fn run() {
-    
+    const LEGAL_AGE: i32 = 18;
+    let age = 16;
+
+    // If/Else
+    if age >= LEGAL_AGE {
+        println!("Bartender: What would you like to drink?");
+    } else {
+        println!("Bartender: Sorry bud, need to be {}. Come back in {} years!", LEGAL_AGE, LEGAL_AGE - age);
+    }
 }
