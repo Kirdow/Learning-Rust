94d6c038232cc982643691dff720d67653cfe6f6
Dennis Rönn <10155603+Kirdow@users.noreply.github.com>
2022-05-19 20:30:27 +0200

    add cli example

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,21 @@
-pub fn run() {
+use std::env;
 
+pub fn run() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Please enter a command!");
+        return;
+    }
+
+    let command = args[1].clone();
+    exec(command);
+}
+
+fn exec(cmd: String) {
+    let result = match cmd.as_str() {
+        "hello" => "Well hello there!",
+        _ => "Sorry, who are you?"
+    };
+
+    println!("{}", result);
 }
