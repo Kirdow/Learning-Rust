fe5ed203a9a75bb4140a8cacc76c0f258d041eb9
Dennis Rönn <10155603+Kirdow@users.noreply.github.com>
2022-05-19 19:59:34 +0200

    add full_name impl fn

--- src/structs.rs
+++ src/structs.rs
@@ -21,6 +21,11 @@ impl Person {
     fn new(first: &str, last: &str) -> Person {
         Person { first_name: first.to_string(), last_name: last.to_string() }
     }
+
+    // Get full name
+    fn full_name(&self) -> String {
+        format!("{} {}", self.first_name, self.last_name)
+    }
 }
 
 pub fn run() {
@@ -43,5 +48,5 @@ pub fn run() {
     println!("TColor: {} {} {}", tc.0, tc.1, tc.2);
 
     let person = Person::new("John", "Doe");
-    println!("Person {} {}", person.first_name, person.last_name);
+    println!("Person {}", person.full_name());
 }
