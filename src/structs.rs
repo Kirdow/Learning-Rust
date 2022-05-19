2e0f7dcf10f4b541bb0ff2e1f1a7b7c9e38fa09b
Dennis Rönn <10155603+Kirdow@users.noreply.github.com>
2022-05-19 19:52:51 +0200

    add tuple struct demo

--- src/structs.rs
+++ src/structs.rs
@@ -7,6 +7,9 @@ struct Color {
     blue: u8
 }
 
+// Tuple Struct
+struct TColor(u8, u8, u8);
+
 pub fn run() {
     let mut c = Color { red: 255, green: 0, blue: 0 };
 
@@ -16,4 +19,8 @@ pub fn run() {
     c.red = 120;
 
     println!("Color: {} {} {}", c.red, c.green, c.blue);
+
+    let tc = TColor(255, 0, 0);
+
+    println!("TColor: {} {} {}", tc.0, tc.1, tc.2);
 }
