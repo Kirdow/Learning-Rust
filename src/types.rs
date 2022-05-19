925651d10dc62d31da5cdd24a3e2c2d3f8345c95
Dennis Rönn <10155603+Kirdow@users.noreply.github.com>
2022-05-19 17:25:38 +0200

    add type info comments to types.rs

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,22 @@
+/*
+Primitive Types--
+
+Integer Types:
+unsigned    u8      u16     u32     u64     u128
+signed      i8      i16     i32     i64     i128
+
+Floating Point Types:
+signed                      f32     f64
+
+Other Types:
+Boolean (bool)
+Characters (char)
+Tuples
+Arrays
+*/
+
+// Hint: Rust is statically typed but compiler may infer the type based on context
+
 pub fn run() {
-    
+
 }
