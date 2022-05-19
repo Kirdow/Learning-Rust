857b1c1967d25d849417bc8d30692a80a56b3ec5
Dennis Rönn <10155603+Kirdow@users.noreply.github.com>
2022-05-19 17:57:33 +0200

    add types code

--- src/types.rs
+++ src/types.rs
@@ -18,5 +18,29 @@ Arrays
 // Hint: Rust is statically typed but compiler may infer the type based on context
 
 pub fn run() {
+    // Default is "i32"
+    let x = 1;
 
+    // Default is "f64"
+    let y = 2.5;
+
+    // Add explicit type
+    let z: i64 = 4040404040404040;
+
+    // Find max size
+    println!("Max u32: {}", std::u32::MAX);
+    println!("Max u64: {}", std::u64::MAX);
+    println!("Max u128: {}", std::u128::MAX);
+
+    // Boolean
+    let is_active: bool = true;
+
+    // Get boolean from expression (inferred)
+    let is_greater = 10 > 5;
+
+    // Character
+    let a1 = 'a';
+    let face = '\u{1F600}';
+
+    println!("{:?}", (x, y, z, is_active, is_greater, face));
 }
